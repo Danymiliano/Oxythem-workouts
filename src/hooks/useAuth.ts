1120e02da5bb16966920ef4bb313d294dda8fe4d
Maxim <38426619+Danymiliano@users.noreply.github.com>
2023-02-24 19:29:40 +0300

    First commit in a new branch

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -2,7 +2,6 @@ import { useSelector } from 'react-redux'
 
 export const useAuth = () => {
   // const { email, token, id } = useSelector()
-
   // return {
   //   isAuth: !!email,
   //   email,
--- src/store/index.ts
+++ src/store/index.ts
@@ -7,5 +7,3 @@ export default configureStore({
     user: userReducer,
   },
 })
-
-

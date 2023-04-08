25c1c100f78ac483207fd051fbbb3a20d21e0dac
Maxim <38426619+Danymiliano@users.noreply.github.com>
2023-04-08 07:56:02 +0300

    Code refactoring

--- src/components/base/Header/Header.tsx
+++ src/components/base/Header/Header.tsx
@@ -3,31 +3,43 @@ import { FC } from 'react'
 
 import { SearchBar } from '@/components/SearchBar/SearchBar'
 import { SvgInline } from '@/components/SvgInline/SvgInline'
+import { useAppDispatch } from '@/hooks/useAppDispatch'
+import { useAuth } from '@/hooks/useAuth'
+import { removeUser } from '@/store/slices/userSlice'
 
 import styles from '../Header/Header.module.scss'
 
-export const Header: FC = (): JSX.Element => (
-  <>
-    <header className={styles.header}>
-      <div className={`${styles['header-inner']} container`}>
-        <Link href='/'>
-          <SvgInline name='logo' width='400' height='43' />
-        </Link>
-        <SearchBar placeholder='Поиск' />
-        <nav>
-          <ul className={styles['nav-wrapper']}>
-            <li className={styles.item}>
-              <Link href='/schedule'>План тренировок</Link>
-            </li>
-            <li className={styles.item}>
-              <Link href='/exercises'>Упражнения</Link>
-            </li>
-            <li className={styles.item}>
-              <Link href='/login'>Вход</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  </>
-)
+export const Header: FC = (): JSX.Element => {
+  const dispatch = useAppDispatch()
+  const { isAuth } = useAuth()
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={`${styles['header-inner']} container`}>
+          <Link href='/'>
+            <SvgInline name='logo' width='400' height='43' />
+          </Link>
+          <SearchBar placeholder='Поиск' />
+          <nav>
+            <ul className={styles['nav-wrapper']}>
+              <li className={styles.item}>
+                <Link href='/schedule'>План тренировок</Link>
+              </li>
+              <li className={styles.item}>
+                <Link href='/exercises'>Упражнения</Link>
+              </li>
+              <li className={styles.item}>
+                {isAuth ? (
+                  <button onClick={() => dispatch(removeUser())}>Выйти</button>
+                ) : (
+                  <Link href='/login'>Вход</Link>
+                )}
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+    </>
+  )
+}
